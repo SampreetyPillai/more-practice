512281ee59fd78474dd2cc3aa18bbca536f0eb46
Collatz-Conjecture315 <81220220+Collatz-Conjecture315@users.noreply.github.com>
2022-12-30 12:26:12 +0530

    more changes

--- sample.cpp
+++ sample.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(){
   
+  int t;
+   cin>>t;
+  for(int i=0;i<t;i++){
+    
+    cout<<"The value of t is <<t<<endl;
+    }
+  
   int myname;
   cin>>myname;
  return 0; 
